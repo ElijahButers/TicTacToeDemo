f071dfc6e9a50cd32aa735a19ffecdcf44b13ed0
ElijahButers <Elijahbuters@gmail.com>
2016-03-04 14:17:39 +0200

    Added two set of ints of player move set.

--- TicTacToeDemo/ViewController.swift
+++ TicTacToeDemo/ViewController.swift
@@ -26,6 +26,8 @@ class ViewController: UIViewController {
     // Properties
     
     let winningCombinations = [[1, 2, 3,], [4, 5, 6], [7, 8, 9],[1, 4, 7], [2, 5, 8], [3, 6, 9], [1, 5, 9], [3, 5, 7]]
+    let playerOneMoves = Set<Int>()
+    let playerTwoMoves = Set<Int>()
     
 
     override func viewDidLoad() {
