c8f66a7b256db0d82522921e2cd7fc22c75d5cf5
ElijahButers <Elijahbuters@gmail.com>
2016-03-04 13:37:10 +0200

    Added array of arrays of winning combinations.

--- TicTacToeDemo/ViewController.swift
+++ TicTacToeDemo/ViewController.swift
@@ -23,6 +23,10 @@ class ViewController: UIViewController {
     @IBOutlet weak var button8: UIButton!
     @IBOutlet weak var button9: UIButton!
     
+    // Properties
+    
+    let winningCombinations = [[1, 2, 3,], [4, 5, 6], [7, 8, 9],[1, 4, 7], [2, 5, 8], [3, 6, 9], [1, 5, 9], [3, 5, 7]]
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
