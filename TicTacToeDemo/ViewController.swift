f62edf1c0ae79294796bc522de3e3217e7a8bff6
ElijahButers <Elijahbuters@gmail.com>
2016-03-04 12:38:58 +0200

    Connected 11 outlets: 10 buttons, 1 label.

--- TicTacToeDemo/ViewController.swift
+++ TicTacToeDemo/ViewController.swift
@@ -9,6 +9,20 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    // Outlets
+    @IBOutlet weak var newGameButton: UIButton!
+    @IBOutlet weak var statusLabel: UILabel!
+    @IBOutlet weak var button1: UIButton!
+    @IBOutlet weak var button2: UIButton!
+    @IBOutlet weak var button3: UIButton!
+    @IBOutlet weak var button4: UIButton!
+    @IBOutlet weak var button5: UIButton!
+    @IBOutlet weak var button6: UIButton!
+    @IBOutlet weak var button7: UIButton!
+    @IBOutlet weak var button8: UIButton!
+    @IBOutlet weak var button9: UIButton!
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
